5987b921d6c1942e4f7a5fca5a7d5f4cba48177c
Dhiwang-ED <awangharispa@gmail.com>
2025-07-03 07:32:53 +0700

    Add files via upload

---
+++ src/pages/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import Latihan from './Latihan';
+
+export default function App() {
+  return (
+    <div className="min-h-screen bg-gray-100 p-4">
+      <h1 className="text-3xl font-bold mb-4 text-center">Latihan Pertidaksamaan Linear</h1>
+      <Latihan />
+    </div>
+  );
+}
